5dc2417f00483b383f9f9125ac5c7211c5eae9b7
abrtx <fgaudino@yahoo.com>
2026-04-12 18:57:00 -0400

    Implement a non-blocking UART

--- src/drivers/uart/uart.c
+++ src/drivers/uart/uart.c
@@ -1,8 +1,16 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #include "uart.h"
 
 #define F_CPU 16000000UL
+
+#define UART_TX_BUFFER_SIZE 64
+
+// -----------------------------
+static volatile char tx_buffer[UART_TX_BUFFER_SIZE];
+static volatile uint8_t tx_head = 0;
+static volatile uint8_t tx_tail = 0;
 
 // -----------------------------
 void uart_init(uint32_t baud) {
@@ -12,7 +20,7 @@ void uart_init(uint32_t baud) {
     UBRR0H = (ubrr >> 8);
     UBRR0L = ubrr;
 
-    // enable TX
+    // enable TX + interrupt
     UCSR0B = (1 << TXEN0);
 
     // 8N1
@@ -20,11 +28,23 @@ void uart_init(uint32_t baud) {
 }
 
 // -----------------------------
+static void uart_start_tx(void) {
+    // enable UDRE interrupt
+    UCSR0B |= (1 << UDRIE0);
+}
+
+// -----------------------------
 void uart_write_char(char c) {
 
-    while (!(UCSR0A & (1 << UDRE0)));
+    uint8_t next = (tx_head + 1) % UART_TX_BUFFER_SIZE;
 
-    UDR0 = c;
+    // wait if buffer full (rare, short)
+    while (next == tx_tail);
+
+    tx_buffer[tx_head] = c;
+    tx_head = next;
+
+    uart_start_tx();
 }
 
 // -----------------------------
@@ -32,5 +52,19 @@ void uart_write_string(const char *str) {
 
     while (*str) {
         uart_write_char(*str++);
+    }
+}
+
+// -----------------------------
+// ISR: send next byte
+// -----------------------------
+ISR(USART_UDRE_vect) {
+
+    if (tx_head == tx_tail) {
+        // buffer empty → disable interrupt
+        UCSR0B &= ~(1 << UDRIE0);
+    } else {
+        UDR0 = tx_buffer[tx_tail];
+        tx_tail = (tx_tail + 1) % UART_TX_BUFFER_SIZE;
     }
 }
